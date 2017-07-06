b5135f3e378defa802f55b818babae0117ff223f
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-07-06 20:11:04 +0530

    NavDrawer Added.

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import RaisedButton from 'material-ui/RaisedButton';
+
+import NavDrawer from '../components/NavDrawer';
 
 injectTapEventPlugin();
 
@@ -10,14 +11,7 @@ class Template extends Component {
 		return(
 			<MuiThemeProvider>
 				<div>
-					<header>
-						<h1>TicTacTuring</h1>
-						<RaisedButton
-							label={'Test Button'}
-							secondary={true}
-							onTouchTap={()=>{console.log('It worked!')}}
-						/>
-					</header>
+					<NavDrawer/>
 					<main>
 						{this.props.children}
 					</main>
