b7d95205083ad5e9741bb6456f5f7b1bf47dfc38
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-07-08 08:13:36 +0530

    Added Konva React-Konva

---
+++ src/containers/TicTacToe.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {Stage} from 'react-konva';
+
+class TicTacToe extends Component {
+
+	state = {
+
+	}
+
+	componentWillMount(){
+		let height = window.innerHeight;
+		let width = window.innerWidth;
+		let size = (height < width) ? height * .8 : width * .8;
+		let rows = this.state.rows;
+		let unit = size / rows;
+
+		this.setState({
+			size,
+			rows,
+			unit
+		});
+	}
+
+	move = () => {
+
+	}
+
+	makeAiMove = () => {
+
+	}
+
+	turingTest = () => {
+
+	}
+
+	recordGame = () => {
+
+	}
+
+	render(){
+		return(
+			<div>
+				<Stage
+					height={}
+					width={}
+				>
+					{/* <Board/> */}
+					{/* <Squares/> */}
+				</Stage>
+			</div>
+		);
+	}
+}
+
+export default TicTacToe;
